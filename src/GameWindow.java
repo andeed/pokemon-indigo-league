3c5ceb715c40a966eb48964dd56e5a66fb3b5e16
Andy Davies <ae.davies@hotmail.co.uk>
2014-08-24 23:12:40 +0100

    Added ability to display images to frame (v0.2.0.0)
    - Software at v0.2.0.0.
    - Added the files 'placeholder.png' and 'pokeball.png'.
    - Added the private function 'displayImageFromFile' which attaches and
    displays the specified file to the visible frame.
    - Added an icon to the window ('pokeball.png').
    - Added a placeholder image to test the display functionality
    ('placeholder.png').
    - Stopped the window from being resizeable.

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -12,7 +12,7 @@
 // (c) 1997-2014 OLM, Inc.                                     //
 // (c) 1998-2006 4Licensing Corporation                        //
 //-------------------------------------------------------------//
-// Version: v0.1.0.1                                           //
+// Version: v0.2.0.0                                           //
 // Updated: 24/08/2014                                         //
 // Authors: Andy Davies                                        //
 // Website: http://www.github.com/andeed/pokemon-indigo-league //
@@ -20,8 +20,13 @@
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class GameWindow extends JFrame {
 	
@@ -30,6 +35,9 @@ public class GameWindow extends JFrame {
 	private final String WINDOW_TITLE = "";
 	private final int    FRAME_WIDTH  = 160;
 	private final int    FRAME_HEIGHT = 144;
+	private final String ICON_FILE    = "./src/pokeball.png"; 
+	
+	private final JLabel frame;
 	
 	public static void main(String[] args) {
 		new GameWindow();
@@ -39,19 +47,35 @@ public class GameWindow extends JFrame {
 		// Set window properties
 		setTitle(WINDOW_TITLE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setIconImage(new ImageIcon(ICON_FILE).getImage());
 		
 		// Create visible game frame
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
-		panel.setBackground(Color.WHITE);
+		frame = new JLabel();
+		frame.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		frame.setBackground(Color.WHITE);
+		frame.setOpaque(true);
+		
+		// Load an image and display it
+		try {
+			File file = new File("./src/placeholder.png");
+			displayImageFromFile(file);
+		} catch (IOException e) {
+			System.err.println("File not found.");
+		}
 		
 		// Add the frame to the window
-		add(panel);
+		add(frame);
 		
 		// Display the window
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+	
+	private void displayImageFromFile(File file) throws IOException {
+		BufferedImage image = ImageIO.read(file);
+		frame.setIcon(new ImageIcon(image));
 	}
 	
 }
